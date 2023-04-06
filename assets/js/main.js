ee2d72311ba7b2ef1ac90d11eb8bd17bdd1a6ec2
Andrea Bellome <andrea.bellome@gmail.com>
2023-04-06 19:54:59 +0200

    end exercise

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+/* start: create an array of different random numbers */
+function randArray(min, max, dim){
+
+    /* INPUT:
+        - min: minimum number
+        - max: maximum number
+        - dim: dimension of the array
+
+       OUTPUT:
+        - randArr: array with different random numbers */
+
+    let randArr = [];
+    if (dim > max){
+        console.log( 'dimension of the array should be less than max. value' ); 
+    } else {
+        for (let i = 0; i < dim; i++){
+            let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+            while (randArr.includes(randomNumber)) {
+                randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+            randArr.push(randomNumber);
+        }
+    }
+
+    return randArr;
+
+}
+/* end: create an array of different random numbers */
+
+/* -------------------------------------- */
+
+/* print on page a list with random numbers */
+let randArr = randArray(1, 100, 5);
+for ( let i = 0; i < randArr.length; i++ ){
+    document.getElementById('randNumbers').innerHTML += `<li>${randArr[i]}</li>`
+}
+
+/* cancel after 2 seconds (2000 ms) */
+setTimeout( function(){
+    document.getElementById('randNumbers').innerHTML = '';
+}, 2000 )
+
+/* generate prompt */
+setTimeout( function(){
+
+    let userNum = [];
+    for ( let i = 0; i < 5; i++ ){
+        var promptnum = parseInt( prompt('Numero:') );
+        userNum.push(promptnum);
+
+        if ( userNum[i] == randArr[i] ){
+
+            document.getElementById('randNumbers').innerHTML += `<li>Il numero selezionato è giusto: ${randArr[i]}</li>`
+
+        } else {
+            document.getElementById('randNumbers').innerHTML += `<li>Il numero selezionato non è giusto. Quello giusto era: ${randArr[i]}</li>`
+        }
+
+    }
+    console.log(userNum)
+
+    
+
+} , 3000 )
+
+
+
+
